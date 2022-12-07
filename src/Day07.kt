31149006bfdd980769521a1a4e8a3927e525f0ed
Sidd <gitProdigy@proton.me>
2022-12-07 00:42:27 -0800

    refactor(day07): cleanup solution

--- src/Day07.kt
+++ src/Day07.kt
@@ -5,9 +5,8 @@ fun main() {
         val root = parse(input)
         var sum = 0L
         fun run(folder: Folder) {
-            if (folder.computedSize!! <= 100000) {
-                sum += folder.computedSize!!
-            }
+            if (folder.size <= 100000L)
+                sum += folder.size
             folder.subDirectories.forEach { run(it) }
         }
         run(root)
@@ -16,22 +15,15 @@ fun main() {
 
     fun part2(input: List<String>): Long {
         val root = parse(input)
-        val unused = 70000000L - root.computedSize!!
-        val delta = max(30000000 - unused, 0)
-
+        val delta = max(30000000L - (70000000L - root.size), 0L)
         var candidate: Folder? = null
         fun run(folder: Folder) {
-            if (folder.computedSize!! > delta) {
-                if (candidate == null || folder.computedSize!! < candidate!!.computedSize!!) {
-                    candidate = folder
-                }
-            }
-
+            if (folder.size >= delta && (candidate == null || folder.size < candidate!!.size))
+                candidate = folder
             folder.subDirectories.forEach { run(it) }
         }
         run(root)
-
-        return candidate!!.computedSize!!
+        return candidate!!.size
     }
 
     // test if implementation meets criteria from the description, like:
@@ -48,37 +40,23 @@ private fun parse(input: List<String>): Folder {
     val root = Folder(null, "")
 
     var current = root
-    input
-        .drop(1)
-        .partitionBy(keepDelim = true) { it.startsWith('$') }
-        .drop(1)
+    input.partitionBy(keepDelim = true) { it.startsWith('$') }
+        .dropWhile { it.isEmpty() }
         .map { it.first().substring(2) to it.drop(1).map { r -> r.split(' ', limit = 2) } }
-        .forEach { (command, result) ->
-            if (command.startsWith("ls")) {
-                result.forEach { (a, name) ->
-                    if ("dir" == a) {
-                        current.subDirectories += Folder(current, name)
-                    } else {
-                        current.files += File(current, a.toLong(), name)
+        .forEach { (cmd, result) ->
+            when (cmd) {
+                "cd /" -> current = root
+                "cd .." -> current = current.parent!!
+                "ls" -> result.forEach { (info, name) ->
+                    when (info) {
+                        "dir" -> current.subDirectories += Folder(current, name)
+                        else -> current.files += File(current, info.toLong(), name)
                     }
                 }
-            } else if (command.startsWith("cd ..")) {
-                current = current.parent!!
-            } else if (command.startsWith("cd ")) {
-                val folderName = command.substring("cd ".length)
-                current = current.subDirectories.first { it.name == folderName }
+
+                else -> cmd.substring("cd ".length).run { current = current.subDirectories.first { it.name == this } }
             }
         }
-
-    // depth first search to calculate folder sizes
-    fun calculateSize(folder: Folder): Long {
-        val fileSum = folder.files.sumOf { it.size }
-        val folderSum = folder.subDirectories.sumOf { calculateSize(it) }
-        val sum = fileSum + folderSum
-        folder.computedSize = sum
-        return sum
-    }
-    calculateSize(root)
 
     return root
 }
@@ -90,5 +68,6 @@ private data class Folder(
     val name: String,
     val files: MutableList<File> = mutableListOf(),
     val subDirectories: MutableList<Folder> = mutableListOf(),
-    var computedSize: Long? = null
-)
+) {
+    val size: Long by lazy { files.sumOf(File::size) + subDirectories.sumOf(Folder::size) }
+}
