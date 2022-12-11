84257a51abe41246ba4f62e1b70e80b640fbe92a
Sidd <gitProdigy@proton.me>
2022-12-11 03:41:38 -0600

    chore: create day 12 template

---
+++ src/Day12.kt
@@ -0,0 +1,18 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        TODO()
+    }
+
+    fun part2(input: List<String>): Int {
+        TODO()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day12_test")
+    check(part1(testInput) == 1)
+//    check(part2(testInput) == 0)
+
+    val input = readInput("Day12")
+    println(part1(input))
+    println(part2(input))
+}
